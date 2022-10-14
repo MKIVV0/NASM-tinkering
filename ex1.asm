0219893287368866e1d0a8f934f1ce7ab889ed6e
Matt <huangzhangying@live.it>
2022-10-15 00:47:14 +0200

    no changes

--- ex1.asm
+++ ex1.asm
@@ -3,4 +3,4 @@ _start:
     mov eax, 1      ; move the integer 1 to the general purpose register eax
     mov ebx, 42     ; exactly as the first instruction
     sub ebx, 29     ; subtract 29 from the 42 stored in eax
-    int 0x80        ; interrupt handler for system calls invoked
+    int 0x80        ; interrupt handler for system calls invoked 
